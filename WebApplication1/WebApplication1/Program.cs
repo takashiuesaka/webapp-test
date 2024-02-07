aca7d901e155205087891f55118e90a0283a8e4d
takashi uesaka <takashi.uesaka@microsoft.com>
2024-02-07 17:41:56 +0900

    Change Response

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -7,7 +7,7 @@ namespace WebApplication1
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapGet("/", () => "Hello World! From GitHub!");
 
             app.Run();
         }
